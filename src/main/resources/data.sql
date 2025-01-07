91df2d84ae7718e7a6e94984808abf4f9fe5178e
Krilov Kirill <ext.kkrylov@tinkoff.ru>
2025-01-08 01:53:25 +0400

    fix commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO users (username, password, role) VALUES ('admin', '$2y$10$xLH/TSmZZwkhagV5j5WDn..v9N1dvVbuwLItiApQ/dZBPI1LqmG2S', 'ROLE_ADMIN');
+INSERT INTO users (username, password, role)
+SELECT 'admin', '$2y$10$xLH/TSmZZwkhagV5j5WDn..v9N1dvVbuwLItiApQ/dZBPI1LqmG2S', 'ROLE_ADMIN'
+WHERE NOT EXISTS (
+    SELECT 1 FROM users WHERE username = 'admin'
+);
